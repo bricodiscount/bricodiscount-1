f16fa1c7bf0c25442e7c8f2e615dc31030fb6567
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2018-12-05 20:36:25 +0000

    Update caisse.py

--- caisse/models/caisse.py
+++ caisse/models/caisse.py
@@ -428,7 +428,7 @@ class ReportVentes(models.AbstractModel):
         cr = self.env.cr
         #location = location[0]
         requete = "SELECT p.barcode, pr.name as produit, l.qty as quantite, l.qty as prixrevient, l.price_unit as prixvente, l.price_unit-l.qty as margeunit, (l.price_unit-l.qty)*l.qty as margetot, l.qty*l.price_unit as ventetot " \
-                  "FROM product_product p, pos_order_line l, pos_order po, product_template pr, ir_property i " \
+                  "FROM product_product p, pos_order_line l, pos_order po, product_template pr " \
                   "WHERE l.product_id = p.id " \
                   "AND p.product_tmpl_id = pr.id " \
                   "AND l.order_id = po.id"
