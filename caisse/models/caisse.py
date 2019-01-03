bb242e1427d6d9f31d3ff41a0a027815bf3e838e
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2019-01-03 12:42:04 +0000

    Update caisse.py

--- caisse/models/caisse.py
+++ caisse/models/caisse.py
@@ -530,52 +530,19 @@ class AccountBankStatementLine(models.Model):
     _name = "account.bank.statement.line"
     _description = "Bank Statement Line"
     _inherit = "account.bank.statement.line"
-    def get_move_lines_for_reconciliation(self, partner_id=None, excluded_ids=None, str=False, offset=0, limit=None, additional_domain=None, overlook_partner=False):
-        """ Return account.move.line records which can be used for bank statement reconciliation.
-
-            :param partner_id:
-            :param excluded_ids:
-            :param str:
-            :param offset:
-            :param limit:
-            :param additional_domain:
-            :param overlook_partner:
-        """
-        if partner_id is None:
-            partner_id = self.partner_id.id
-
-        # Blue lines = payment on bank account not assigned to a statement yet
-        reconciliation_aml_accounts = [self.journal_id.default_credit_account_id.id, self.journal_id.default_debit_account_id.id]
-        domain_reconciliation = ['&',  ('statement_line_id', '=', False), ('account_id', 'in', reconciliation_aml_accounts)]
-
-        # Black lines = unreconciled & (not linked to a payment or open balance created by statement
-        domain_matching = [('reconciled', '=', False)]
-        if partner_id or overlook_partner:
-            domain_matching = expression.AND([domain_matching, [('account_id.internal_type', 'in', ['payable', 'receivable'])]])
-        else:
-            # TODO : find out what use case this permits (match a check payment, registered on a journal whose account type is other instead of liquidity)
-            domain_matching = expression.AND([domain_matching, [('account_id.reconcile', '=', True)]])
-
-        # Let's add what applies to both
-        #domain = expression.OR([domain_reconciliation, domain_matching])
-        domain = domain_reconciliation
-        if partner_id and not overlook_partner:
-            domain = expression.AND([domain, [('partner_id', '=', partner_id)]])
-
-        # Domain factorized for all reconciliation use cases
-        if str:
-            str_domain = self.env['account.move.line'].domain_move_lines_for_reconciliation(str=str)
-            if not partner_id:
-                str_domain = expression.OR([str_domain, [('partner_id.name', 'ilike', str)]])
-            domain = expression.AND([domain, str_domain])
-        if excluded_ids:
-            domain = expression.AND([[('id', 'not in', excluded_ids)], domain])
-
-        # Domain from caller
-        if additional_domain is None:
-            additional_domain = []
-        else:
-            additional_domain = expression.normalize_domain(additional_domain)
-        domain = expression.AND([domain, additional_domain])
-
-        return self.env['account.move.line'].search(domain, offset=offset, limit=limit, order="date_maturity desc, id desc")
+    def _get_common_sql_query(self, overlook_partner = False, excluded_ids = None, split = False):
+        acc_type = "acc.reconcile = true"
+        select_clause = "SELECT aml.id "
+        from_clause = "FROM account_move_line aml JOIN account_account acc ON acc.id = aml.account_id "
+        account_clause = ''
+        if self.journal_id.default_credit_account_id and self.journal_id.default_debit_account_id:
+            account_clause = "(aml.statement_id IS NULL AND aml.account_id = self.journal_id.default_credit_account_id)"
+        where_clause = """WHERE aml.company_id = %(company_id)s
+                          AND (
+                                    """ + account_clause + """
+                          )"""
+        where_clause = where_clause + ' AND aml.partner_id = %(partner_id)s' if self.partner_id else where_clause
+        where_clause = where_clause + ' AND aml.id NOT IN %(excluded_ids)s' if excluded_ids else where_clause
+        if split:
+            return select_clause, from_clause, where_clause
+        return select_clause + from_clause + where_clause
