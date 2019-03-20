584320b92631ef43be490166d5360f7476a6ab2a
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2019-03-20 19:34:57 +0000

    Update etats_caisse.py

--- caisse/wizard/etats_caisse.py
+++ caisse/wizard/etats_caisse.py
@@ -192,5 +192,5 @@ class PosStocks(models.TransientModel):
             raise UserError(_("You have to set a logo or a layout for your company."))
         elif (not self.env.user.company_id.external_report_layout_id):
             raise UserError(_("You have to set your reports's header and footer layout."))
-        data = {'date_start': self.start_date, 'date_stop': self.end_date, 'location': self.location.id, 'nomlocation':self.location.display_name, 'categorie':self.categorie.id, 'avecm': self.avecm}
+        data = {'date_start': self.start_date, 'date_stop': self.end_date, 'location': self.location.id, 'nomlocation':self.location.display_name, 'categorie':self.categorie, 'avecm': self.avecm}
         return self.env.ref('caisse.sale_stocks_report').report_action([], data=data)
