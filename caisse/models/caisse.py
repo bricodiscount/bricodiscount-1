1cc725739ceae79a1257d525ce195f4cedddf099
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2019-03-20 20:06:52 +0000

    Update caisse.py

--- caisse/models/caisse.py
+++ caisse/models/caisse.py
@@ -459,6 +459,7 @@ class ReportStocks(models.AbstractModel):
                   "on p.id = sortie.product_id " \
                   "left join final " \
                   "on p.id = final.id "+cond+" " \
+                  "where categ_id = '"+str(categorie)+"' " \
                   "order by pr.name"
         #raise UserError(_(requete))
         cr.execute(requete)
